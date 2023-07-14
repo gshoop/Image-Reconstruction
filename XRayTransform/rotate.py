Fixed code, works for point source however needs normalization

XRayTransform/rotate.py
@@ -56,10 +56,10 @@ def radon_transform(image, thetas, n):
     Computes Radon transform of image
 
     inputs: image: input image (dtype: numpy-ndarray)
-    thetas: array containing angles of rotation
-    n: size of image
+            thetas: array containing angles of rotation
+            n: size of image
     '''
-    #n = image.shape[0]
+    
     rt_img = np.uint8(np.zeros((image.shape[0],np.size(thetas))))
 
     for th in range(np.size(thetas)):
@@ -67,35 +67,30 @@ def radon_transform(image, thetas, n):
 
         for i in range(n):
             Sum0 = (2/n)*sum(rot_img[i,:])
-            #Sum1 = (2/n)*sum(rot_img[i,:,1])
-            #Sum2 = (2/n)*sum(rot_img[i,:,2])
-
-            rt_img[i,th] = Sum0
-            #rt_img[i,th,1] = Sum1
-            #rt_img[i,th,2] = Sum2
+            rt_img[n-1-i,th] = Sum0
 
     return rt_img
 
 def backproject(rt, thetas, n):
     '''
     Computes backprojection using sinogram of original image
-    inputs: rt: input sinogram
-    thetas: array containing angles of rotation
-    n: size of image
+    inputs: rt: input sinogram (dtype: numpy-ndarray)
+            thetas: array containing angles of rotation
+            n: size of image
     '''
 
     img_recon = np.uint8(np.zeros((n,n)))
     temp = np.uint8(np.zeros((n,n)))
-    for th in range(np.size(thetas)):
-        #img_recon = naive_image_rotate(img_recon,thetas[1]-thetas[0],'same')
+    
 
+    for th in range(np.size(thetas)):
         for i in range(n):
             temp[i,:] = rt[:,th]
-            #temp[i,:,1] = rt[:,th,1]
-            #temp[i,:,2] = rt[:,th,2]
 
-        temp = naive_image_rotate(temp,thetas[th],'same')
-        img_recon += temp
+        temp = naive_image_rotate(temp,-thetas[th],'same')
+
+        img_recon += temp                                   # Normalization needed
+        # Need to take core of when pixel values overflow over 255
 
     return img_recon
 
@@ -103,35 +98,34 @@ def backproject(rt, thetas, n):
     
 
 if __name__=='__main__':
-    #filename = 'mickey.png'
+
     rot_start = 0.01
-    rot_end = 360
-    steps = 256
+    rot_end = 180
+    steps = 100
     image = cv2.imread(r'/home/swuupii/ImageReconstruction/XRayTransform/mickey.png',0)
     n = image.shape[0]
-    thetas = np.linspace(rot_start,rot_end,steps)
+    thetas = np.linspace(rot_start,rot_end,steps)   # Thetas in degrees since naive_image_rotation converts to rad
     
-    rt = radon_transform(image,thetas,n)
-    
-    print(rt.shape)
+    new_img = np.zeros((n,n))
+    new_img[20,9] = 255
+
+    # Perform radon_transform on original image then backproject the sinogram 'rt'
+    rt = radon_transform(new_img,thetas,n)
     img = backproject(rt,thetas,n)
-    
-    #print(image[:,32,:])
-    #rotated_image = naive_image_rotate(image,30,'full')
-    #cv2.imshow("original image", image)
-    #cv2.imshow("sinogram",rt)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
 
     plt.figure(1)
-    plt.imshow(image,cmap='gray')
+    plt.imshow(new_img,cmap='gray')
 
     plt.figure(2)
     plt.imshow(rt,cmap='gray')
     x_ticks = np.linspace(0, np.pi, 3)
-    x_labels = ['0', 'π/2', 'π']
-    plt.xticks(np.linspace(0, 256, len(x_ticks)), x_labels)
+    x_labels = ['0', 'π', '2π']
+    plt.xticks(np.linspace(0, len(thetas), len(x_ticks)), x_labels)
 
     plt.figure(3)
     plt.imshow(img,cmap='gray')
     plt.show()
+
+    #plt.figure(4)
+    #plt.imshow(new_img,cmap='gray')
+    #plt.show()