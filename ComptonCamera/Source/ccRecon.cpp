52432fb2be6b8429b7af997eec4fd5618d5e8bcc
grshoop <greysonshoop@gmail.com>
2023-07-22 12:22:04 -0700

    Created Source dir with ccRecon.cpp for simple compton camera implementation

---
+++ ComptonCamera/Source/ccRecon.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char const *argv[])
+{
+    // Need to define the FOV (or source space) and voxelize
+    float x_begin, x_end, y_begin, y_end, z_begin, z_end;
+
+    
+
+
+
+    return 0;
+}
