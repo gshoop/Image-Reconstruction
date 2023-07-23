84a377f8b51df277ac06b380abf366c8f6b3259d
grshoop <greysonshoop@gmail.com>
2023-07-22 20:03:07 -0700

    Added ConstructCones function prototype in ccFunctions.h

--- ComptonCamera/Headers/ccFunctions.h
+++ ComptonCamera/Headers/ccFunctions.h
@@ -8,3 +8,5 @@ using namespace std;
 
 void DefinePostionMatrix( vector<vector<vector<vector<float> > > > &pos_matrix, float* f, unsigned x_vox, unsigned y_vox, unsigned z_vox,
                           float x_start, float y_start, float z_start, float delx, float dely, float delz );
+
+void ConstructCones( float* conelist_1D, string input);
